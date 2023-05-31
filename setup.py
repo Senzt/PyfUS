1c05a0fcd958b05cdb5b36c14eababddd953430b
Waradon Senzt <waradonai@gmail.com>
2023-05-31 23:11:49 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,9 +1,32 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name='pyfus',
-    version='0.1',
-    packages=find_packages(),
-    description='A Python package',
-    install_requires=[],
+# Reads the content of your README.md into a variable to be used in the setup below
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pyfus',                           # should match the package folder
+    packages=['pyfus'],                     # should match the package folder
+    version='0.1',                                # important for updates
+    license='MIT',                                  # should match your chosen license
+    description='Testing installation of Package',
+    long_description=long_description,              # loads your README.md
+    long_description_content_type="text/markdown",  # README.md is of type 'markdown'
+    author='Waradon Senzt',
+    author_email='waradonai@gmail.com',
+    url='https://github.com/Senzt/PyfUS', 
+    install_requires=['numpy'],                  # list all packages that your package uses
+    keywords=["pypi", "pyfus", "fUS"],         #descriptive meta-data
+    classifiers=[                                   # https://pypi.org/classifiers
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Documentation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    
+    download_url="",
 )
